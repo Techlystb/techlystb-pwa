606d435bb47e351f9eb51dc31e5150dc5d10fea2
Techlystb blogs <techlystb@gmail.com>
2025-06-28 03:27:37 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,29 @@
-const CACHE_NAME = 'techlystb-cache-v1';
+const CACHE_NAME = 'techlystb-cache-v2';
 const urlsToCache = [
   '/',
   '/manifest.json',
   '/icon-192.png',
-  '/icon-512.png'
+  '/icon-512.png',
+  '/offline.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request)
+      .then(response => response || caches.match('/offline.html')))
   );
 });
